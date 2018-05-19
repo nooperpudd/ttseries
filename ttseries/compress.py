28bb3bff2e9fd001a183e7aa607ae4d254aa448b
wangweichen <weichen.wang@jsxfedu.com>
2018-05-19 12:21:39 +0800

    compress

--- ttseries/compress.py
+++ ttseries/compress.py
@@ -1,1 +1,24 @@
 # encoding:utf-8
+import abc
+
+
+class BaseCompress(abc.ABCMeta):
+
+    @abc.abstractmethod
+    def compress(self, obj, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def decompress(self, obj, *args, **kwargs):
+        pass
+
+
+class Lz4Compress(BaseCompress):
+    """
+    """
+
+    def compress(self, obj, *args, **kwargs):
+        pass
+
+    def decompress(self, obj, *args, **kwargs):
+        pass
